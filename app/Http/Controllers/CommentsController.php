fbf5c84f1bc5f9c975a6fd4589de6eddb0c311ee
rayan <rhrayan2228@gmail.com>
2022-11-23 21:25:02 +0600

    add comment table and validation

---
+++ app/Http/Controllers/CommentsController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use Illuminate\Http\Request;
+
+class CommentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            '*'=> "required",
+        ]);
+        // Comment::insert([
+        //     'post_slug' => 
+        // ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Comment  $comments
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Comment $comments)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Comment  $comments
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Comment $comments)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Comment  $comments
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Comment $comments)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Comment  $comments
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Comment $comments)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_11_23_205459_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('post_slug');
+            $table->integer('user_id');
+            $table->integer('parent_id')->nullable();
+            $table->longText('comment')->nullable();
+            $table->string('comment_status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
